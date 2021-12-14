05f4423dff0431e1fd374e9b2473ec7f463caadc
David Möller <davidm@student.chalmers.se>
2021-12-14 11:49:31 +0100

    Solved day 14

--- day14/index2.r
+++ day14/index2.r
@@ -1,12 +1,23 @@
 library("stringr")
 input <- read.csv("./day14/input.txt", header = T)
-polymer <- "NNCB"
-inserts <- matrix("", length(input[,1]), 3)
+polymer <- "KBKPHKHHNBCVCHPSPNHF"
+inserts <- matrix("", length(input[,1]), 4)
 
 for(i in 1:(length(input[,1]))) {
   row <- str_split(input[i,1], " -> ")[[1]]
   key_split <- str_split(row[1], "")[[1]]
-  inserts[i,] <- c(row[1], paste(key_split[1], row[2], sep = ""), paste(row[2], key_split[2], sep = ""))
+  inserts[i,] <- c(row[1], paste(key_split[1], row[2], sep = ""), paste(row[2], key_split[2], sep = ""), row[2])
+}
+
+chars <- c()
+for(i in inserts[,1]) {
+  chars <- c(chars, str_split(i, "")[[1]])
+}
+chars <- unique(chars)
+
+char_count <- rep(0, length(chars))
+for(i in 1:length(chars)) {
+  char_count[i] <- str_count(polymer, chars[i])
 }
 
 counts <- rep(0, length(inserts[,1]))
@@ -15,46 +26,33 @@ for(i in 2:str_length(polymer)) {
   if(identical(integer(0), index)) {
     index <- length(counts) + 1
     counts <- c(counts, 0)
-    inserts[i,] <- rbind(inserts, c(substr(polymer, i-1, i), "", ""))
+    inserts[i,] <- rbind(inserts, c(substr(polymer, i-1, i), "", "", ""))
   }
   counts[index] <- counts[index] + 1
 }
 
-for(i in 1:1) {
+for(i in 1:40) {
   new_count <- rep(0, length(counts))
   for(k in 1:length(counts)) {
+    char_index <- which(chars == inserts[k,4])
+    char_count[char_index] <- char_count[char_index] + counts[k]
+    
     dest <- which(inserts[,1] == inserts[k,2])
     if(!identical(integer(0), dest)) {
       new_count[dest] <- new_count[dest] + counts[k]
     } else if (inserts[k,2] != "") {
       new_count <- c(new_count, counts[k])
-      inserts <- rbind(inserts, c(inserts[k, 2], "", ""))
+      inserts <- rbind(inserts, c(inserts[k, 2], "", "", ""))
     }
     dest <- which(inserts[,1] == inserts[k,3])
     if(!identical(integer(0), dest)) {
       new_count[dest] <- new_count[dest] + counts[k]
     } else if (inserts[k,3] != "") {
       new_count <- c(new_count, counts[k])
-      inserts <- rbind(inserts, c(inserts[k, 3], "", ""))
+      inserts <- rbind(inserts, c(inserts[k, 3], "", "", ""))
     }
   }
   counts <- new_count
 }
 
-chars <- c()
-for(i in inserts[,1]) {
-  chars <- c(chars, str_split(i, "")[[1]])
-}
-chars <- unique(chars)
-char_count <- rep(0, length(chars))
-
-for(i in 1:length(chars)) {
-  for(k in 1:length(inserts[,1])) {
-    
-    if(str_count(inserts[k,1], chars[i]) > 0) {
-      char_count[i] <- char_count[i] + counts[k]
-    }
-  }
-}
-
-max(char_count) - min(char_count)
+print(max(char_count) - min(char_count), digits = 10)
